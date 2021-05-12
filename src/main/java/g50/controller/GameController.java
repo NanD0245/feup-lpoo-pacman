ca0085ce0adcb22263381b5c335b81d6654b239c
Bruno Mendes <brod56@sapo.pt>
2021-05-12 11:53:29 +0100

    Refactor controllers

--- src/main/java/g50/controller/GameController.java
+++ src/main/java/g50/controller/GameController.java
@@ -1,8 +1,6 @@
 package g50.controller;
 
 import g50.gui.GUI;
-import g50.gui.GUIObserver;
-import g50.model.element.movable.PacMan;
 import g50.model.element.movable.ghost.Ghost;
 import g50.model.map.GameMap;
 import g50.view.GameMapViewer;
@@ -10,64 +8,41 @@ import g50.view.GameMapViewer;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class GameController implements GUIObserver, Controller {
-
+public class GameController implements Controller{
     private final GameMap map;
     private final List<GhostController> ghostsController;
     private final PacManController pacManController;
-    private int framerate = 60;
-    private Timer timer;
-    private TimerTask updater;
     private final GameMapViewer viewer;
+    private int score;
+    private final GUI gui;
 
-    public GameController(GameMap map, GameMapViewer viewer){
+    public GameController(GUI gui, GameMapViewer viewer, GameMap map, int score) {
         this.map = map;
-        this.viewer = viewer;
         this.ghostsController = new ArrayList<>();
-        for(Ghost ghost: map.getGhosts()) this.ghostsController.add(new GhostController(this.map, ghost));
+        for(Ghost ghost: map.getGhosts()){
+            this.ghostsController.add(new GhostController(this.map, ghost));
+        }
         this.pacManController = new PacManController(map);
+        this.score = score;
+        this.viewer = viewer;
+        this.gui = gui;
     }
-
-    public void setUp(){
-        setUp(framerate);
-    }
-
-    public void setUp(int framerate){
-        this.framerate = framerate;
-        updater = new TimerTask() {
-            int frame = 0;
-            @Override
-            public void run() {
-                frame++;
-                update(frame);
-            }
-        };
-        timer = new Timer();
-        timer.schedule(updater, 1000/framerate, 1000/framerate);
-    }
-
-    public void terminate() {
-        timer.cancel();
-    }
-
-    @Override
-    public void addPendingAction(GUI.ACTION action) {
-        if(action == GUI.ACTION.QUIT) terminate();
-        pacManController.addPendingAction(action);
-    }
-
 
     @Override
     public void update(int frame) {
         pacManController.update(frame);
-        for(GhostController ghostController: ghostsController) ghostController.update(frame);
+        for (GhostController ghostController: ghostsController){
+            ghostController.update(frame);
+        }
         try {
-            viewer.draw();
+            viewer.draw(gui, map);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void addPendingAction(GUI.ACTION action){
+        pacManController.addPendingAction(action);
     }
 }
